41d15004fdb0f17d804242154381dcc01595ed8e
pljay <35439240+pljay@users.noreply.github.com>
2025-11-26 16:02:53 +0800

    Create App.vue with RuleConfig as main view

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <RuleConfig />
+  </div>
+</template>
+
+<script>
+import RuleConfig from './RuleConfig.vue';
+
+export default {
+  components: {
+    RuleConfig
+  }
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
